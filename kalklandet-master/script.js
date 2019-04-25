1413514f00365c8495c0fc94bca5f38208037dce
Emilie H.B.S.B Madsbjerg <EmilieMadsbjerg@Emilies-MacBook-Pro-4.local>
2019-04-25 20:03:09 +0200

    ey

---
+++ kalklandet-master/script.js
@@ -0,0 +1,15 @@
+document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	console.log("Toogle menu");
+	document.querySelector("#menu").classList.toggle("hidden");
+
+	let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+
+	if (erSkjult == true) {
+		document.querySelector("#menuknap").textContent = "☰";
+	} else {
+		document.querySelector("#menuknap").textContent = "X";
+	}
+}
